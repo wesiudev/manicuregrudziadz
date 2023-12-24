c0c38bc59fffa148fde0b18236321d496f324d19
wesselpawel <wesselpawel@users.noreply.github.com>
2023-12-24 15:21:48 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,18 @@ import Reserve from "@/components/Reserve";
 import carousel from "@/public/carousel.json";
 import { ServicesGrid } from "@/components/ServicesGrid";
 
-async function getServicesList() {
-  const req = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/services`);
-  const services = req.json();
-  return services;
-}
+// async function getServicesList() {
+//   const req = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/services`);
+//   const services = req.json();
+//   return services;
+// }
 
 export default async function Home() {
-  const { services } = await getServicesList();
+  // const { services } = await getServicesList();
   return (
     <div className="w-full">
       <Hero />
-      <ServicesGrid services={services} />
+      {/* <ServicesGrid services={services} /> */}
       <Reserve />
       <About carousel={carousel.carousel} />
     </div>
