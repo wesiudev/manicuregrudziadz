0b41906f8472af482ac988c63d1ae50fc16d55a5
wesselpawel <wesselpawel@wp.pl>
2024-04-21 04:35:11 +0200

    header causing navigation problems

--- app/(blog)/blog/layout.tsx
+++ app/(blog)/blog/layout.tsx
@@ -1,15 +1,7 @@
-import Footer from "@/components/Footer";
-import { Header } from "@/components/Header";
-
 export default function ShopLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <div className="bg-white overflow-hidden">
-      <Header />
-      {children}
-    </div>
-  );
+  return <div className="bg-white overflow-hidden">{children}</div>;
 }
