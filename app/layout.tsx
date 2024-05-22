1680514b0acd2f828af98b839b12f2136b6d193c
wesselpawel <wesselpawel@wp.pl>
2024-05-22 23:36:38 +0200

    deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
-import { Parisienne } from "next/font/google";
-import "./globals.css";
-import localFont from "next/font/local";
-import { Metadata } from "next";
-import { Header } from "@/components/Header";
-import Footer from "@/components/Footer";
-import Toast from "@/components/Toast";
+// import { Parisienne } from "next/font/google";
+// import "./globals.css";
+// import localFont from "next/font/local";
+// import { Metadata } from "next";
+// import { Header } from "@/components/Header";
+// import Footer from "@/components/Footer";
+// import Toast from "@/components/Toast";
 import { getDocuments } from "@/firebase";
 
 export const metadata: Metadata = {
@@ -29,53 +29,53 @@ export default async function RootLayout({
         className={`mt-[75px] ${cocosharp.variable} ${parisienne.variable}
        w-full overflow-x-hidden`}
       >
-        <Header services={services} />
-        <Toast />
+        {/* <Header services={services} />
+        <Toast /> */}
         {children}
-        <Footer services={services} />
+        {/* <Footer services={services} /> */}
       </body>
     </html>
   );
 }
 
-const parisienne = Parisienne({
-  weight: "400",
-  variable: "--font-persisienne",
-  subsets: ["latin"],
-});
-const cocosharp = localFont({
-  src: [
-    {
-      path: "../public/fonts/Italic.ttf",
-      weight: "400",
-      style: "italic",
-    },
-    {
-      path: "../public/fonts/BoldItalic.ttf",
-      weight: "700",
-      style: "italic",
-    },
-    {
-      path: "../public/fonts/Bold.ttf",
-      weight: "700",
-    },
-    {
-      path: "../public/fonts/ExtraLight.ttf",
-      weight: "200",
-    },
-    {
-      path: "../public/fonts/Light.ttf",
-      weight: "300",
-    },
-    {
-      path: "../public/fonts/LightItalic.ttf",
-      weight: "300",
-      style: "italic",
-    },
-    {
-      path: "../public/fonts/Regular.ttf",
-      weight: "500",
-    },
-  ],
-  variable: "--font-cocosharp",
-});
+// const parisienne = Parisienne({
+//   weight: "400",
+//   variable: "--font-persisienne",
+//   subsets: ["latin"],
+// });
+// const cocosharp = localFont({
+//   src: [
+//     {
+//       path: "../public/fonts/Italic.ttf",
+//       weight: "400",
+//       style: "italic",
+//     },
+//     {
+//       path: "../public/fonts/BoldItalic.ttf",
+//       weight: "700",
+//       style: "italic",
+//     },
+//     {
+//       path: "../public/fonts/Bold.ttf",
+//       weight: "700",
+//     },
+//     {
+//       path: "../public/fonts/ExtraLight.ttf",
+//       weight: "200",
+//     },
+//     {
+//       path: "../public/fonts/Light.ttf",
+//       weight: "300",
+//     },
+//     {
+//       path: "../public/fonts/LightItalic.ttf",
+//       weight: "300",
+//       style: "italic",
+//     },
+//     {
+//       path: "../public/fonts/Regular.ttf",
+//       weight: "500",
+//     },
+//   ],
+//   variable: "--font-cocosharp",
+// });
